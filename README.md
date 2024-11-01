99492d2afb84bcc414d89304fa60d3dfbb4dd760
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-11-01 09:11:53 -0500

    initial commit: ksdump, the basics work.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Usage
+# About
 
+Dumps all `<binary>` files to json using `<format>` files. It captures
+enum value names and instance values. It does not handle ksy imports.
+
+**Why?** The Kaitai Struct Web IDE parsing and export to json choke on large
+files because of browser memory limits and the Kaitai Visualizer's `ksdump`
+chokes on some of my formats that otherwise are compiled and iterated over just
+fine using python or javascript. `ksdump.js` uses `json-stream-stringify` to
+avoid maximum string length limits and uses `require-from-string` instead of
+compiling and then writing out the parsers just to read them back in (which is
+why it doesn't handle ksy imports).
+
+On a Ryzen 5700G it is capable of parsing a 165MB binary file using a format
+consisting of 200+ `id` fields having nested types and instanced values in ~4s
+and writing out the 666MB compact json on a 980 Pro SSD in ~30s and formatting
+to a 1.11GB file in ~78s.
+
+
+## Usage
+
+Usage: `npm install && node ksdump <format> <input> <outpath> [--format]`
+
+The binary filename associated with a format is taken from the format's meta
+fields as `${id}.${file-extension}`.
+
+### Inputs
+
+If `<format>` is a directory then `<binary>` must also be a directory. Each
+`*.ksy` format file with a matching format meta associated filename will be
+dumped.
+
+If `<format>` is a file and `<binary>` is a file the format's meta associated
+filename is ignored.
+
+If `<format>` is a file and `<binary>` is a directory then the format's meta
+associated filename is searched for in `${binary}`.
+
+### Outputs
+
+The json filename will be taken from the format's meta field as `${id}.json`
+unless `<binary>` is a file, in which case it will be `${binary_stem}.json`.
+
+Output is 'compact' by default, `--format` invokes `jq` to format. `jq` is
+provided by `node-jq` and is located at `./node_modules/node-jq/bin/jq.exe`.
+With `--format` a tmp file is created and `jq` is called equivalently to
+`jq '.' tmp.json > formatted.json`.
---
+++ ksdump.js
@@ -0,0 +1,238 @@
+import fs from "fs";
+import path from "path";
+import { execSync } from 'child_process';
+
+import { JsonStreamStringify } from 'json-stream-stringify';
+import KaitaiStruct from "kaitai-struct";
+import KaitaiStructCompiler from "kaitai-struct-compiler";
+import "node-jq";
+import require_from_string from "require-from-string";
+import signale_pkg from 'signale';
+import yaml from "yaml";
+
+
+const { Signale } = signale_pkg;
+const logger = new Signale({
+    types: {
+        export: {
+            badge: '📤',
+            label: 'Exporting',
+            color: 'cyan',
+        },
+        format: {
+            badge: '📝',
+            label: 'Formatting with jq:',
+            color: 'cyan',
+        },
+        generate: {
+            badge: '⚙️',
+            label: 'Generating:',
+            color: 'cyan',
+        },
+        parse: {
+            badge: '🔍',
+            label: 'Parsing binary:',
+            color: 'cyan',
+        },
+        populate: {
+            badge: '🔍',
+            label: 'Populating enum/instance values:',
+            color: 'cyan',
+        },
+        process: {
+            label: 'Processing:',
+            color: 'cyan',
+        },
+        skip: {
+            badge: '⤵️',
+            label: 'Skipping:',
+            color: 'green',
+        },
+        success: {
+            badge: '✅',
+            label: 'Success',
+            color: 'green',
+        },
+        error: {
+            badge: '❌',
+            label: 'Error',
+            color: 'red',
+        },
+    },
+});
+
+
+function getFilestem(filepath) {
+    const filename = path.basename(filepath);
+    const lastIndex = filename.lastIndexOf(".");
+    return lastIndex > 0 ? filename.substring(0, lastIndex) : filename;
+}
+
+async function generateJavascriptParser(ksyContent) {
+    logger.generate(`${ksyContent.meta.id}`);
+    const compiler = KaitaiStructCompiler;
+    const compiled = await compiler.compile("javascript", ksyContent);
+
+    const parserName = ksyContent.meta.id + ".js";
+    const parserKey = Object.keys(compiled).find((key) => key.toLowerCase() === parserName.toLowerCase());
+    return compiled[parserKey];
+}
+
+async function parseInputFile(parser, binaryFile) {
+    logger.parse(`${binaryFile}`);
+    const inputBinary = fs.readFileSync(binaryFile);
+    const inputBuffer = Buffer.from(inputBinary);
+    const inputStream = new KaitaiStruct.KaitaiStream(inputBuffer, 0);
+
+    const GeneratedParser = require_from_string(parser);
+    const parserName = Object.keys(GeneratedParser)[0];
+    const ParserConstructor = GeneratedParser[parserName];
+    const parsed = new ParserConstructor(inputStream);
+
+    const enumNames = Object.getOwnPropertyNames(ParserConstructor).filter((name) => {
+        const value = ParserConstructor[name];
+        return typeof value === "object" && value !== null && typeof value._read !== "function";
+    });
+
+    const processParsedData = (data) => {
+        if (Array.isArray(data)) {
+            data.forEach(processParsedData);
+        } else if (data !== null && typeof data === "object") {
+            // Instantiate instance members, these virtual properties create
+            // new properties prefixed with `_m_`.
+            const prototype = Object.getPrototypeOf(data);
+            if (prototype) {
+                Object.getOwnPropertyNames(prototype).forEach((prop) => {
+                    if (!prop.startsWith('_')) {
+                        const _ = data[prop];
+                    }
+                });
+            }
+
+            // Populate enum value names, perhaps even in an instantiated instance.
+            for (const key in data) {
+                if (key.startsWith("_") && !key.startsWith("_m_")) continue;
+                const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+                if (enumNames.includes(capitalizedKey)) {
+                    const enumValues = ParserConstructor[capitalizedKey];
+                    data[key] = { name: enumValues[data[key]], value: data[key] };
+                } else {
+                    processParsedData(data[key]);
+                }
+            }
+        }
+    };
+
+    logger.populate(`${binaryFile}`);
+    processParsedData(parsed);
+
+    return parsed;
+}
+
+async function exportToJson(parsedData, jsonFile, format = false) {
+    logger.export(`${jsonFile}`);
+
+    const filterObject = (obj) => {
+        if (Array.isArray(obj)) {
+            return obj.map(filterObject);
+        } else if (obj !== null && typeof obj === 'object') {
+            return Object.keys(obj).reduce((acc, key) => {
+                if (key.startsWith('_m_')) {
+                    // Instantiated instance member.
+                    acc[key.slice(3)] = filterObject(obj[key]);
+                } else if (!key.startsWith('_')) {
+                    acc[key] = filterObject(obj[key]);
+                }
+                return acc;
+            }, {});
+        } else {
+            return obj;
+        }
+    };
+
+    const filteredData = filterObject(parsedData);
+    const stringifyStream = new JsonStreamStringify(filteredData);
+    const outputStream = fs.createWriteStream(jsonFile);
+
+    return new Promise((resolve, reject) => {
+        stringifyStream
+            .pipe(outputStream)
+            .on('finish', async () => {
+                if (format) {
+                    try {
+                        logger.format(`${jsonFile}`);
+                        const tmpFile = `${jsonFile}.tmp`;
+                        fs.renameSync(jsonFile, tmpFile);
+
+                        const jqPath = "./node_modules/node-jq/bin/jq.exe";
+                        const command = `"${jqPath}" . "${tmpFile}" > "${jsonFile}"`;
+                        execSync(command, { stdio: 'inherit' });
+
+                        fs.unlinkSync(tmpFile);
+                        logger.success(`${jsonFile}`);
+                        console.log();
+                    } catch (error) {
+                        logger.error("Error formatting JSON:", error);
+                        return reject(error);
+                    }
+                } else {
+                    logger.success(`${jsonFile}`);
+                    console.log();
+
+                }
+                resolve();
+            })
+            .on('error', error => {
+                logger.error("Error writing JSON file:", error);
+                reject(error);
+            });
+    });
+}
+
+(async function main() {
+    logger.time('ksdump');
+    console.log();
+
+    if (process.argv.length < 5) {
+        console.log("Usage: node ksdump <format> <input> <outpath> [--format]");
+        return;
+    }
+
+    const [, , formatPath, inputPath, outputPath, formatFlag] = process.argv;
+    const formatOption = formatFlag === "--format";
+
+    const parseYAML = (yamlFile) => {
+        const yamlData = fs.readFileSync(yamlFile, 'utf-8');
+        return yaml.parse(yamlData);
+    };
+
+    logger.process(`${formatPath}`)
+    const promises = [];
+    if (fs.statSync(formatPath).isDirectory()) {
+        const ksyFiles = fs.readdirSync(formatPath).filter(file => file.endsWith(".ksy"));
+        for (const ksyFile of ksyFiles) {
+            const ksyContent = await parseYAML(path.join(formatPath, ksyFile));
+            const parser = await generateJavascriptParser(ksyContent);
+            const binaryFilename = `${ksyContent.meta.id}.${ksyContent.meta["file-extension"]}`;
+            const binaryFile = path.join(inputPath, binaryFilename);
+            if (!fs.existsSync(binaryFile)) {
+                logger.skip(`${binaryFilename} not found for format ${ksyFile}.`);
+                continue;
+            }
+            const parsedData = await parseInputFile(parser, binaryFile);
+            promises.push(exportToJson(parsedData, path.join(outputPath, `${ksyContent.meta.id}.json`), formatOption));
+        };
+    } else {
+        const ksyContent = await parseYAML(formatPath);
+        const parser = await generateJavascriptParser(ksyContent);
+        const binaryFilename = `${ksyContent.meta.id}.${ksyContent.meta["file-extension"]}`;
+        const binaryFile = fs.statSync(inputPath).isDirectory()
+            ? path.join(inputPath, binaryFilename)
+            : inputPath;
+        const parsedData = await parseInputFile(parser, binaryFile);
+        promises.push(exportToJson(parsedData, `${getFilestem(binaryFile)}.json`, formatOption));
+    }
+
+    await Promise.all(promises);
+    logger.timeEnd('ksdump');
+})();
